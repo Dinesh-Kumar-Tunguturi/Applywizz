415b4d362c4c4a914d47dd55358d0c178632e3db
Dinesh-Kumar-Tunguturi <dineshkumartunguturi@gamil.com>
2025-08-15 12:22:29 +0530

    Use first-party Python runtime; remove legacy runtime field

--- api/index.py
+++ api/index.py
@@ -1,7 +1,8 @@
 # api/index.py
 import os
 from asgiref.wsgi import WsgiToAsgi
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Full_web.settings")  # <-- change PROJECT_NAME
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Full_web.settings")  # <- change name
 from django.core.wsgi import get_wsgi_application
 
 app = WsgiToAsgi(get_wsgi_application())
